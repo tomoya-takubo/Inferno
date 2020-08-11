512a637cd8c449269bfd8baba80ab2b50404ad17
tomoya-takubo <tkb1003@gmail.com>
2020-08-11 21:48:29 +0900

    20200811 初回コミット
    プロジェクト作成、プレイヤー設置、火山岩ジェネレータ設置

---
+++ Assets/Scripts/FireStoneGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireStoneGenerator : MonoBehaviour
+{
+    // 火山岩プレハブ
+    public FireStone fireStonePrefab;
+
+    // 上向きに加える力
+    public float addForce;
+
+    // 生成する時間間隔
+    public float generateInterval;
+
+    IEnumerator Start()
+    {
+        // 火山岩生成
+        while(true)
+        {
+            // 火山岩生成
+            FireStone fireStone = Instantiate(fireStonePrefab, this.transform);
+
+            // Z軸方向に回転
+            fireStone.transform.rotation = Quaternion.Euler(fireStone.transform.rotation.x
+                                                            , fireStone.transform.rotation.y
+                                                            , Random.Range(-5f, -25f));
+
+            // 速度を与える
+            fireStone.GetComponent<Rigidbody2D>().AddForce(fireStone.transform.up * addForce);
+
+            // ***秒待つ
+            yield return new WaitForSeconds(generateInterval);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 移動速度最大値
+    public float maxPlayerVelocity;
+
+    void Update()
+    {
+        // 横移動
+        Move();
+    }
+
+    private void Move()
+    {
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * maxPlayerVelocity, 0);
+    }
+}
